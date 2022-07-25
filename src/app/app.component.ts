0d23e271058bf835e287512178b9c896160b29ec
Nueng <onetwotee1@gmail.com>
2022-07-25 16:41:36 +0700

    Pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,12 +33,26 @@ export class AppComponent {
       started: new Date(2021, 4, 13)
     }
   ];
+  filteredStatus = '';
+  appStatus = new Promise<string>((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve('stable');
+    }, 2000);
+  })
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
+  }
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(2022, 7, 21)
+    })
   }
 
 }
